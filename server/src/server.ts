23d0c9ba9eb81968b9616f8bde0b017884fcb754
ThalesZZ <thazars@hotmail.com>
2023-06-03 02:54:31 -0300

    started eslint

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,10 @@ const PORT = 3000
 
 const app = express()
 
+app.get('/', (req, res) => {
+  res.send('Server is up!')
+})
+
 app.listen(PORT, () => {
-    console.log('Running on port: '+PORT)
+  console.log('Server is up on port: ' + PORT)
 })
