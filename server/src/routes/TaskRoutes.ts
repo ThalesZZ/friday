bcbbbce1e4d5f49d9b6d2f66ffa40ddc1cc0dbaa
ThalesZZ <thazars@hotmail.com>
2023-06-03 07:01:14 -0300

    finished tasks routes

--- server/src/routes/TaskRoutes.ts
+++ server/src/routes/TaskRoutes.ts
@@ -21,3 +21,49 @@ export default express
       return res.send(e)
     }
   })
+
+  .post('/tasks', async (req, res) => {
+    try {
+      const bodySchema = z.object({
+        label: z.string().nonempty(),
+        description: z.coerce.string(),
+      })
+      const { label, description } = bodySchema.parse(req.body)
+
+      const task = await prisma.task.create({ data: { label, description } })
+      return task
+    } catch (e) {
+      return res.send(e)
+    }
+  })
+
+  .put('/tasks/:id', async (req, res) => {
+    try {
+      const paramsSchema = z.object({ id: z.string().uuid() })
+      const { id } = paramsSchema.parse(req.params)
+
+      const bodySchema = z.object({
+        label: z.string().nonempty(),
+        description: z.coerce.string(),
+      })
+      const { label, description } = bodySchema.parse(req.body)
+
+      const task = await prisma.task.update({
+        where: { id },
+        data: { label, description },
+      })
+
+      return task
+    } catch (e) {
+      return res.send(e)
+    }
+  })
+
+  .delete('/tasks/:id', async (req, res) => {
+    const paramsSchema = z.object({ id: z.string().uuid() })
+    const { id } = paramsSchema.parse(req.params)
+
+    await prisma.task.delete({ where: { id } })
+
+    return res.status(200)
+  })
